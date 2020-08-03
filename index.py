531cc361d62d7c5531b4a8864584404b92d0cd57
zch <943339564@qq.com>
2020-08-03 11:19:54 +0800

    元组

--- index.py
+++ index.py
@@ -1,22 +1,3 @@
-myList = ['Monday','Tuesday',1,2]
-myList.remove('Monday')
-print(myList) # ['Tuesday', 1, 2]
+tup1 = (1,2,3,4,5,6)
 
-myList = ['Monday','Tuesday',1,2]
-myList.pop(0)
-print(myList)  # ['Tuesday', 1, 2]
-
-myList = ['Monday','Tuesday',1,2]
-del myList[0]
-print(myList)  # ['Tuesday', 1, 2]
-
-# del myList     # 删除后，这 个list 没了
-# print(myList)  # NameError: name 'myList' is not defined
-
-
-myList = ['Monday','Tuesday',1,2]
-myList.clear()
-print(myList)    # []
-
-
-
+print(tup1*2)   # NameError: name 'tup1' is not defined
